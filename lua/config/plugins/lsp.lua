f21e81d818663f3a09ad5c5fa9b0a42988b818d3
hauck95 <hauck95@gmail.com>
2025-06-28 20:04:36 +0200

    godot works now

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,7 +1,33 @@
  return {
     --nvim lspconfig
+    { "mfussenegger/nvim-dap",
+config = function ()
+    local dap = require("dap")
+    dap.adapters.godot = {
+        type = "server",
+        host = "127.0.0.1",
+        port = 6006,
+}
+
+dap.configurations.gdscript = {
+	{
+		type = "godot",
+		request = "launch",
+		name = "Launch scene",
+		project = "${workspaceFolder}",
+		launch_scene = true,
+	},
+}
+end
+},
     {
     "neovim/nvim-lspconfig",
+    config = function ()
+        require("lspconfig")["gdscript"].setup({
+    	name = "godot",
+    	cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
+    })
+    end
     },
 
     {
